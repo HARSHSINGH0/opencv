30f18b5afe4c205f44b60fe1c7c1cff77254c232
HARSH SINGH <hs475052@gmail.com>
2021-01-10 10:05:35 +0530

    threshold testing

---
+++ threshold.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import numpy as np
+img=cv.imread('photos/cat.jpg',0)
+#here ret is return value sometime it is placed as "_"
+ret, thresh1 = cv.threshold(img,50,255,cv.THRESH_BINARY) 
+ret, thresh2 = cv.threshold(img,50,255,cv.THRESH_BINARY_INV) 
+ret, thresh3 = cv.threshold(img,50,255,cv.THRESH_TRUNC) 
+ret, thresh4 = cv.threshold(img,50,255,cv.THRESH_TOZERO) 
+ret, thresh5 = cv.threshold(img,50,255,cv.THRESH_TOZERO_INV) 
+  
+ 
+cv.imshow('Binary Threshold', thresh1) 
+cv.imshow('Binary Threshold Inverted', thresh2) 
+cv.imshow('Truncated Threshold', thresh3) 
+cv.imshow('Set to 0', thresh4) 
+cv.imshow('Set to 0 Inverted', thresh5) 
+
+cv.imshow("Image",img)
+cv.waitKey(0)
+cv.destroyAllWindows()
